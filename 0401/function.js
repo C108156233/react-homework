3a059ab19243cf31069f6d4dee1c9c7637273e2e
C108156233 <80021035+C108156233@users.noreply.github.com>
2021-04-01 12:03:33 +0800

    0401
    課堂練習

---
+++ 0401/function.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Car() {
+  return <h2>Hi, I am also a Car!</h2>;
+}
+
+ReactDOM.render(<Car />, document.getElementById('root'));
+
+localhost:3000
