96b5a647c39ffd56ef564b097e8ab3c2a187005d
C108156233 <80021035+C108156233@users.noreply.github.com>
2021-04-08 10:28:30 +0800

    0401
    0401

---
+++ 0401/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'reacr-dom';
+import app from './app';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <app />
+    </React.StrictMode>
+    document.getElementById('root')
+);
